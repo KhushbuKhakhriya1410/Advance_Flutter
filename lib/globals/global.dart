3b699362024e07dabbbfc1239a903ae4d8e82ce9
KhushbuKhakhriya1410 <sem3b.25.tmtbca@gmail.com>
2022-11-09 10:48:54 +0530

    media booster

---
+++ lib/globals/global.dart
@@ -0,0 +1,127 @@
+class Global {
+  static List<Map> allCategories = [
+    {
+      'category': 'Anime',
+      'imagePath': 'assets/images/anime1.jpg',
+    },
+    {
+      'category': 'Anime',
+      'imagePath': 'assets/images/anime2.jpg',
+    },
+    {
+      'category': 'Anime',
+      'imagePath': 'assets/images/anime3.png',
+    },
+    {
+      'category': 'Anime',
+      'imagePath': 'assets/images/anime4.jpg',
+    },
+    {
+      'category': 'Anime',
+      'imagePath': 'assets/images/anime5.jpg',
+    },
+    {
+      'category': 'Car',
+      'imagePath': 'assets/images/car1.jpg',
+    },
+    {
+      'category': 'Car',
+      'imagePath': 'assets/images/car2.jpg',
+    },
+    {
+      'category': 'Car',
+      'imagePath': 'assets/images/car3.jpg',
+    },
+    {
+      'category': 'Car',
+      'imagePath': 'assets/images/car4.jpg',
+    },
+    {
+      'category': 'Car',
+      'imagePath': 'assets/images/car5.jpg',
+    },
+    {
+      'category': 'Interior',
+      'imagePath': 'assets/images/house1.jpg',
+    },
+    {
+      'category': 'Interior',
+      'imagePath': 'assets/images/house2.jpg',
+    },
+    {
+      'category': 'Interior',
+      'imagePath': 'assets/images/house3.jpg',
+    },
+    {
+      'category': 'Interior',
+      'imagePath': 'assets/images/house4.jpg',
+    },
+    {
+      'category': 'Interior',
+      'imagePath': 'assets/images/house5.jpg',
+    },
+    {
+      'category': 'Marvel',
+      'imagePath': 'assets/images/marvel1.jpg',
+    },
+    {
+      'category': 'Marvel',
+      'imagePath': 'assets/images/marvel2.jpg',
+    },
+    {
+      'category': 'Marvel',
+      'imagePath': 'assets/images/marvel3.jpg',
+    },
+    {
+      'category': 'Marvel',
+      'imagePath': 'assets/images/marvel4.jpg',
+    },
+    {
+      'category': 'Marvel',
+      'imagePath': 'assets/images/marvel5.jpg',
+    },
+    {
+      'category': 'Nature',
+      'imagePath': 'assets/images/nature1.jpg',
+    },
+    {
+      'category': 'Nature',
+      'imagePath': 'assets/images/nature2.jpg',
+    },
+    {
+      'category': 'Nature',
+      'imagePath': 'assets/images/nature3.jpg',
+    },
+    {
+      'category': 'Nature',
+      'imagePath': 'assets/images/nature4.jpg',
+    },
+    {
+      'category': 'Nature',
+      'imagePath': 'assets/images/nature5.jpg',
+    },
+  ];
+
+  static List<Map> homePage = [
+    {
+      'category': 'Anime',
+      'imagePath': 'assets/images/anime1.jpg',
+    },
+    {
+      'category': 'Car',
+      'imagePath': 'assets/images/car1.jpg',
+    },
+    {
+      'category': 'Nature',
+      'imagePath': 'assets/images/nature5.jpg',
+    },
+    {
+      'category': 'House',
+      'imagePath': 'assets/images/House5.jpg',
+    },
+    {
+      'category': 'Marvel',
+      'imagePath': 'assets/images/marvel5.jpg',
+    },
+  ];
+}
